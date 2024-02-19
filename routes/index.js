f01abf2e1a69896f54448016c93bd1ad4fdfecba
Gunasekhar16 <98088622+Gunasekhar16@users.noreply.github.com>
2024-02-18 19:24:51 -0600

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'contack Database application', message: 'welcome to the contack daatabase application with is made using node.js and express' });
-});
-
-module.exports = router;
